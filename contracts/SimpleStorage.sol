e9f0b7b18ef7c9e62b0f6a5aa5baf65d025878e9
Anas <muhammedanas5186@gmail.com>
2025-02-12 07:43:58 +0530

    simple storage contract and test case using Mocha

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -2,6 +2,19 @@
 
 pragma solidity ^0.8.28;
 
-contract SimpleStorage{
-    
+contract SimpleStorage {
+    string private _data;
+
+    constructor(string memory data) {
+        _data = data;
+    }
+
+    function setData(string memory data) public{
+        _data = data;
+    }
+
+    function getData() public view returns(string memory) {
+        return _data;
+    }
 }
+ 
---
+++ test/SimpleStorage.test.ts
@@ -0,0 +1,25 @@
+import { expect } from "chai";
+import {
+    time,
+    loadFixture,
+} from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import hre from 'hardhat';
+
+describe("SimpleStorage Contract", function () {
+
+    async function deploySimpleStorageFixture() {
+        const SSInstance = await hre.ethers.getContractFactory("SimpleStorage");
+        const SSDeployedInstance = await SSInstance.deploy("StringToStore");
+        return { SSDeployedInstance };
+    }
+
+    describe("Deployment", function () {
+        it("Should set the right string to store", async function () {
+            const { SSDeployedInstance } = await loadFixture(deploySimpleStorageFixture);
+
+            await SSDeployedInstance.setData("StringToStore");
+            expect(await SSDeployedInstance.getData()).to.equal("StringToStore");
+        })
+    })
+
+})  
